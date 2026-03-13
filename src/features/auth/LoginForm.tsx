71a9ee54cb5e2c3bad1a6fcf4ec4df7e3780f3ba
Joaquin Gonzalez <joaquin.gonzalez@maccio.com>
2026-03-13 17:42:52 -0300

    ref

--- src/features/auth/LoginForm.tsx
+++ src/features/auth/LoginForm.tsx
@@ -47,6 +47,7 @@ export function LoginForm() {
           type="email"
           autoComplete="email"
           error={!!errors.email}
+          value={import.meta.env.DEV ? import.meta.env.VITE_DEFAULT_EMAIL : ""}
           {...register('email')}
         />
       </FormField>
@@ -57,6 +58,7 @@ export function LoginForm() {
           type="password"
           autoComplete="current-password"
           error={!!errors.password}
+          value={import.meta.env.DEV ? import.meta.env.VITE_DEFAULT_PASSWORD : ""}
           {...register('password')}
         />
       </FormField>
